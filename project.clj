e0d46594afbaabf8723a7b7861199b8d4c5339db
Rose Molina <r.molina.atienza@gmail.com>
2018-08-23 18:03:55 +0100

    Updare to clojure 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring "1.7.0-RC1"]
                  [compojure "1.6.1"]]
 
